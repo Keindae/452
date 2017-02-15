00e539625742257286bd0dfb4d3188914ae439a4
Keindae <noblettm@mail.gvsu.edu>
2017-02-14 21:48:00 -0500

    Folder

--- 5/sample_program1.c
+++ 5/sample_program1.c
@@ -11,15 +11,20 @@ int main ()
 { 
    int shmId; 
    char *shmPtr;
+   struct shmid_ds test;
 
    if ((shmId = shmget (IPC_PRIVATE, FOO, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) { 
       perror ("i can't get no..\n"); 
       exit (1); 
    } 
-   if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) { 
+   if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) {
       perror ("can't attach\n"); 
       exit (1); 
    }
+
+   shmctl(shmId, IPC_STAT, &test);
+   int size = (int) test.shm_segsz;
+   printf("Size of memory: %d or %#x\n", size, size);
    printf ("value a: %lu\t value b: %lu\n", (unsigned long) shmPtr, (unsigned long) shmPtr + FOO);
    if (shmdt (shmPtr) < 0) { 
       perror ("just can't let go\n"); 
