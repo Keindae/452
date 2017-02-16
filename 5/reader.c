1587ea0b8df2fbdb30d089ff3f9439f2ca470be2
Ben <commetb@localhost.localdomain>
2017-02-16 12:58:46 -0500

    almost working

--- 5/reader.c
+++ 5/reader.c
@@ -9,12 +9,16 @@
 
 #define MEMORY_SIZE 4096
 void exit_handler (int sigNum);
+int shmId, flId;
+char *shared_mem;
+int *flag_mem;
 int main(){
 	int shmid;
-	key_t key;
-	char *shared_mem, *s;
-
+	key_t key, flag_key;
+	char *s;
 	key = 4444;
+	flag_key = 1234;
+	signal (SIGINT, exit_handler);
 
 	if((shmid = shmget(key, MEMORY_SIZE, 0666)) < 0){
 		perror("shmget failed");
@@ -26,15 +30,39 @@ int main(){
 		exit(1);
 	}
 
-	for(s = shared_mem; *s != NULL; s++){
-		putchar(*s);
+	if((flId = shmget(flag_key, MEMORY_SIZE, 0666)) < 0){
+		perror("shmget failed");
+		exit(1);
 	}
-	putchar('\n');
 
-	*shared_mem = '*';
+	if((flag_mem = shmat(flId, NULL, 0)) == (void *) - 1){
+		perror("shmat failed");
+		exit(1);
+	}
 
+	while(1){
+		if(flag_mem[0] == 0){
+			for(s = shared_mem; *s != NULL; s++){
+				putchar(*s);
+			}
+			flag_mem[0] = 1;
+		}
+		if(flag_mem[1] == 0){
+			for(s = shared_mem; *s != NULL; s++){
+				putchar(*s);
+			}
+			flag_mem[1] = 1;
+		}
+	}
+	shmdt(shared_mem);
 	shmdt(shared_mem);
 	exit(0);
+}
 
-
+void exit_handler (int sigNum)
+{
+	shmdt(shared_mem);
+	shmdt(flag_mem);
+	printf ("That's it, I'm shutting you down\n");
+	exit(0);
 }
--- 5/writer.c
+++ 5/writer.c
@@ -9,55 +9,69 @@
 
 #define MEMORY_SIZE 4096
 void exit_handler (int sigNum);
-int shmId;
-char *shared_mem;
+int shmId, flId;
+char *shared_meme;
+int *flag_meme;
 int main(){
 	//Listen for ^C
 	signal (SIGINT, exit_handler);
 	char input[256];
-	key_t key;
+	key_t key, flag_key;
 	char *s;
 
+	flag_key = 1234;
 	key = 4444;
 
 	if ((shmId = shmget(key, MEMORY_SIZE, IPC_CREAT | 0666)) < 0) {
-	        perror("shmget");
+	        perror("shmget failed");
 	        exit(1);
 	 }
 
-	 if ((shared_mem = shmat(shmId, NULL, 0)) == (char *) -1) {
+	 if ((flId = shmget(flag_key, MEMORY_SIZE, IPC_CREAT | 0666)) < 0) {
+					 perror("shmget failed");
+					 exit(1);
+		}
+
+	 if ((shared_meme = shmat(shmId, NULL, 0)) == (char *) -1) {
         perror("shmat");
         exit(1);
     }
+		if ((flag_meme = shmat(flId, NULL, 0)) == (void *) -1) {
+         perror("shmat");
+         exit(1);
+     }
 
 
 	while(1){
-		s = shared_mem;
+		s = shared_meme;
 		fprintf(stderr, "Enter string: ");
 		//Get input from user
 		fgets(input, 256, stdin);
 		//Spawn in a new in the rand_sleep method and send it our user input
-		fprintf(stderr, "%s\n", input);
 
 		int i;
 		for(i = 0; i < strlen(input); i++){
 			*s++ = input[i];
 		}
-
-		while (*shared_mem != '*' && *(shared_mem + 1) != 1)
-        	sleep(1);
-
+		flag_meme[0] = 0;
+		flag_meme[1] = 0;
+		while (!flag_meme[0] && !flag_meme[1]){
+        sleep(1);
+		}
 	}
-
-	shmdt(shared_mem);
+	shmdt(shared_meme);
 	shmctl(shmId, IPC_RMID, NULL);
+	shmdt(flag_meme);
+	shmctl(flId, IPC_RMID, NULL);
 	return 0;
 }
 
 void exit_handler (int sigNum)
 {
-	shmdt(shared_mem);
+	shmdt(shared_meme);
 	shmctl(shmId, IPC_RMID, NULL);
+	shmdt(flag_meme);
+	shmctl(flId, IPC_RMID, NULL);
 	printf ("That's it, I'm shutting you down\n");
 	exit(0);
 }
