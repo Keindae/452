bc76509215c237aeab362caac1418afc20e1bffa
Ben Commet <bencommet@gmail.com>
2017-03-16 12:25:42 -0400

    changes

---
+++ 7/lab7.c
@@ -0,0 +1,31 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(){
+    __int64 start_counter= 0;
+    double frequency = 0.0;
+    int i;
+    LARGE_INTEGER li;
+    if(!QueryPerformanceFrequency(&li)){
+        printf("Query performance frequency done broke\n");
+        exit(0);
+    }
+    printf("Frequency of high-resolution Counter: %lf\n", double(li.QuadPart));
+    printf("Miliseconds per tick: %lf\n", 1000.0/double(li.QuadPart));
+    frequency = double(li.QuadPart)/1000.0;
+    QueryPerformanceCounter(&li);
+    start_counter = li.QuadPart;
+
+    for(i = 0; i < 1000000; i++){
+        ;
+    }
+    QueryPerformanceCounter(&li);
+    printf("%lf\n", double(li.QuadPart - start_counter)/frequency);
+
+    LARGE_INTEGER temp;
+    QueryPerformanceCounter(&li);
+    start_counter = li.QuadPart;
+    QueryPerformanceCounter(&li);
+    printf("%lf\n", double(li.QuadPart - start_counter)/frequency);
+
+}
